9a703a1ba5377a6850a0d29ff98346306bf5e31d
Mirka McIntyre <mirka.kiana@gmail.com>
2025-09-15 10:43:30 -0400

    Finished lesson 3

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+    return (
+        <main>
+            <h1>About this site</h1>
+            <p>We are using it to learn React with Next.js</p>
+        </main>
+    )
+}
