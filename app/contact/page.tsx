c30a6dfa5aaa86924c9716f968b2e74ea3bb4d74
Mirka McIntyre <mirka.kiana@gmail.com>
2025-09-22 11:43:15 -0400

    Added comments and finished lab.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,10 +9,17 @@ export default function Contact() {
     const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setName(event.target.value);
     }
+    // create var for message 
+   const [message, setMessage] = useState<string>('');
+    const handleMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setMessage(event.target.value);
+    }
+    // create var for message
+ const [confirmation, setConfirmation] = useState<string>('');
 
     // show confirmation when button clicked
     const handleSubmit = () => {
-        const [message, confirmation] = useState<string>('');
+         setConfirmation(`Your Name: ${name} Nice to meet you, ${message} `)
     }
 
     return (
@@ -21,7 +28,16 @@ export default function Contact() {
             <p>Contact us at 705.000.0000 or send us a message below</p>
             <input type="text" placeholder="Your Name" value={name} onChange={handleNameChange} />
             <p>Your name is: {name}</p>
+
+            {/* Make new input for the message*/}
+            <input type="text" placeholder="Message" value={message} onChange={handleMessageChange}/>
+            <p>Your message is: {message}</p>
             <button onClick={handleSubmit}>Submit</button>
+            <p>{confirmation}</p>
         </main>
     );
+
+       
+
+
 }
