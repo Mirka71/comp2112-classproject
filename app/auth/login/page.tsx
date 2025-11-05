bbac70f2aa3cc85b66cadc378ed92fc0e0306caa
Mirka McIntyre <mirka.kiana@gmail.com>
2025-11-04 23:46:51 -0500

    Lab 4- made state variable called message and added form inputs.

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,17 +1,35 @@
 'use client';
 import PageTitle from "../../components/PageTitle"
 
-
+// import useState in order to create a state var
+import { useState } from "react";
 // Lab 4 - Login Form
 
 // Login function
 
 export default function Login() {
+
+    // Make state variable
+    const [message, setMessage] = useState<string>('');
+
     return(
         <section>
         <PageTitle title="Login" />
         
             <h1>Login</h1>
+
+        <form>
+            {/* username text input */}
+            <input type="text" placeholder="Enter your username"/>
+
+            {/* password input */}
+            <input type="password" placeholder="Please enter your password"/>
+
+            {/* submit button */}
+            <button type="submit"/>
+
+        </form>
+
         </section>
 
     )
